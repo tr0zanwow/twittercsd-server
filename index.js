096ba73684804bc4d00c8fe6080fcbe9c52fd703
tr0zanwow <pranil.rathod93@gmail.com>
2019-11-24 20:09:08 +0530

    asdsad

--- index.js
+++ index.js
@@ -39,23 +39,9 @@ const userActivityWebhook = twitterWebhooks.userActivity({
       console.log(dataReceived)
     })();
 
-    userActivityWebhook.subscribe({
-        userId: process.env.TWITTER_USER_ID,
-        accessToken: process.env.TWITTER_ACCESS_TOKEN,
-        accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    })
-    .then(function (userActivity) {
-        userActivity
-        .on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
-        .on ('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
-        .on ('follow', (data) => console.log (userActivity.id + ' - follow'))
-        .on ('mute', (data) => console.log (userActivity.id + ' - mute'))
-        .on ('revoke', (data) => console.log (userActivity.id + ' - revoke'))
-        .on ('direct_message', (data) => console.log (userActivity.id + ' - direct_message'))
-        .on ('direct_message_indicate_typing', (data) => console.log (userActivity.id + ' - direct_message_indicate_typing'))
-        .on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
-        .on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
-    });
+    (async () =>{
+      userActivityWebhook.on ('event', (event, userId, data) => console.log (userId + ' Event Occured'));
+    })();
 
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {
