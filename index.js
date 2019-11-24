e3d57397cbd8f5ae51ebf3efe543e106f7a57bf6
tr0zanwow <pranil.rathod93@gmail.com>
2019-11-24 17:25:15 +0530

    define endpoint

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({ typeDefs, resolvers,playground: {
   },
   tabs: [
     {
-      endpoint,
+      endpoint:'/playground',
       query: defaultQuery,
     },
   ],
