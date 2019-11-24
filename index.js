5bfe7665eefa00b200d4ce30c3b6a3b5001939b2
tr0zanwow <pranil.rathod93@gmail.com>
2019-11-24 21:46:07 +0530

    subscription response

--- index.js
+++ index.js
@@ -41,11 +41,23 @@ const userActivityWebhook = twitterWebhooks.userActivity({
         })();
 
     (async () =>{
-      const promise = userActivityWebhook.userActivityWebhook.subscribe({
+      const promise = userActivityWebhook.subscribe({
         userId: process.env.TWITTER_USER_ID,
         accessToken: process.env.TWITTER_ACCESS_TOKEN,
         accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
+    })
+    .then(function (userActivity) {
+      userActivity
+      .on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
+      .on ('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
+      .on ('follow', (data) => console.log (userActivity.id + ' - follow'))
+      .on ('mute', (data) => console.log (userActivity.id + ' - mute'))
+      .on ('revoke', (data) => console.log (userActivity.id + ' - revoke'))
+      .on ('direct_message', (data) => console.log (userActivity.id + ' - direct_message'))
+      .on ('direct_message_indicate_typing', (data) => console.log (userActivity.id + ' - direct_message_indicate_typing'))
+      .on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
+      .on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
+  });
       const dataReceived = await promise;
       console.log('Subscriptions Response')
       console.log(dataReceived)
