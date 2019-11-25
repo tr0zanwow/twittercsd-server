593040dbeb8c6fee6010144b19eecab58d159bbe
tr0zanwow <pranil.rathod93@gmail.com>
2019-11-25 10:33:16 +0530

    added auto subscription

--- index.js
+++ index.js
@@ -84,11 +84,30 @@ io.on('connection', (socket) => {
       users.push(temp)
   }
     console.log(users)
+
+    (async () =>{
+      await userActivityWebhook.subscribe({
+        userId: data.userTwitterId,
+        accessToken: data.access_token,
+        accessTokenSecret: data.access_secret
+    })
+    .then(function (userActivity) {
+        userActivity
+        .on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
+        .on ('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
+        .on ('follow', (data) => console.log (userActivity.id + ' - follow'))
+        .on ('mute', (data) => console.log (userActivity.id + ' - mute'))
+        .on ('revoke', (data) => console.log (userActivity.id + ' - revoke'))
+        .on ('direct_message', (data) => console.log (userActivity.id + ' - direct_message'))
+        .on ('direct_message_indicate_typing', (data) => console.log (userActivity.id + ' - direct_message_indicate_typing'))
+        .on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
+        .on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
+    });
+      })();
   });
 
   userActivityWebhook.on ('event',function (event, userId, data){ 
     console.log (data)
-    
   });
  
   socket.on('disconnect', () => {
