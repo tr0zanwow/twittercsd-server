8f677e046dfe67b91be035be7c576ebbe6e0131c
tr0zanwow <pranil.rathod93@gmail.com>
2019-11-27 01:18:06 +0530

    deleted socket

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const typeDefs = require('./schemas.js')
 const resolvers = require('./resolvers.js')
 const cors = require('cors')
 const app = express();
-const socket = require('socket.io')
 const https = require("https");
-var users = [];
 
 setInterval(function() {
   https.get("https://apollo-graphql-socket-node.herokuapp.com/");
 }, 300000);
-
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
 
 app.use(bodyParser.json());
 
@@ -31,6 +27,12 @@ const userActivityWebhook = twitterWebhooks.userActivity({
   app
 });
 
+userActivityWebhook.on('event',function (event, userId, data){ 
+
+});
+
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
+
 server.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 4000;
@@ -41,49 +43,3 @@ const expressServer = app.listen(PORT, () => {
   console.log(`Listening at http://${host}:${port}`);
 });
 
-var io = socket(expressServer);
-
-io.on('connection', (socket) => {
-  console.log('Client connected on socket with ID:', socket.id);
-  
-  userActivityWebhook.on('event',function (event, userId, data){ 
-    if(users.find(x => x.twitterID === userId)){
-      var tempIndx = users.findIndex(x => x.twitterID === userId);
-      
-      console.log(event)
-      io.to(users[tempIndx].socketID).emit('eventOccured',event);
-      // io.to(users[tempIndx].socketID).emit('normalizedUserData',tweetUserNormalized);
-    }
-  });
-
-  socket.on('disconnect',function(){
-    console.log('Client disconnected with ID:', socket.id);
-  });
-
-  socket.on('creds',function(data){
-    const temp = {
-      socketID : socket.id,
-      twitterID : data.userTwitterId,
-      accessToken : data.access_token,
-      accessTokenSecret : data.access_secret
-    }
-    if(users.find(x => x.twitterID === data.userTwitterId)){
-      var index1 = users.findIndex(x => x.twitterID === data.userTwitterId);
-      users[index1] = temp;
-  }
-  else{
-      users.push(temp)
-  }
-    console.log(users);
-
-    (async function() {
-      await userActivityWebhook.subscribe({
-        userId: data.userTwitterId,
-        accessToken: data.access_token,
-        accessTokenSecret: data.access_secret
-    }).then(function (userActivity) {});
-  })();
-
-  });
-  
-  });
