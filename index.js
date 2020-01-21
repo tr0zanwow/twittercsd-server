267702706903a149145220a2bae675edfd9d798e
Pranil Rathod <rathod1337@gmail.com>
2020-01-21 21:59:02 +0530

    pagination test

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const resolvers = {
         });
         const searchData = await promise;
         if(searchData.search_metadata.next_results == null){
-          throw new graphQLErrors('no more data found')
+          throw new ApolloError("No Data Found");
           // console.log('no more data found')
         }
         else{
